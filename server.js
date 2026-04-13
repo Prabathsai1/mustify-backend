8dd9b74f525e666cc9d43ddd3dbd39277860d1b8
Prabathsai1 <saiprabathnagireddy@gmail.com>
2026-04-13 15:44:27 +0530

    added port

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require('./src/app.js')
 const connect = require('./src/db/db.js')
+const PORT = process.env.PORT || 8080;
 
+// app.get('/', (req, res) => {
+//     res.json("server started")
+// })
 
-app.listen(3000, console.log("server is running successfully"))
+app.listen(PORT, console.log("server is running successfully"))
 connect()
